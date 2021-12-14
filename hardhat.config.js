8478c7c660f15530be5f7c6fd6da2c5a440f9177
Muntasir-Mahmud <m.siyam529@gmail.com>
2021-12-14 18:02:43 +0600

    Added NFT and NFTMarket contracts

--- hardhat.config.js
+++ hardhat.config.js
@@ -1,21 +1,21 @@
 require("@nomiclabs/hardhat-waffle");
+const fs = require("fs");
+const privateKey = fs.readdirSync(".secret").toString();
 
-// This is a sample Hardhat task. To learn how to create your own go to
-// https://hardhat.org/guides/create-task.html
-task("accounts", "Prints the list of accounts", async (taskArgs, hre) => {
-  const accounts = await hre.ethers.getSigners();
 
-  for (const account of accounts) {
-    console.log(account.address);
-  }
-});
-
-// You need to export an object to set up your config
-// Go to https://hardhat.org/config/ to learn more
-
-/**
- * @type import('hardhat/config').HardhatUserConfig
- */
 module.exports = {
-  solidity: "0.8.4",
+    networks: {
+        hardhat: {
+            chainId: 1337,
+        },
+        mumbai: {
+            url: "https://polygon-mumbai.g.alchemy.com/v2/dpW6cbFtqSFgum7dVtsCN3LgXF2QrFcj",
+            accounts: [privateKey]
+        },
+        mainnet: {
+            url: "https://polygon-mainnet.g.alchemy.com/v2/RN3BV8eUjMNezxe2-C9MgptIrnavVt49",
+            accounts: [privateKey]
+        }
+    },
+    solidity: "0.8.4",
 };
