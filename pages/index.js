6a9a497597535003e5aaaca454a9137ec04061f7
Muntasir-Mahmud <m.siyam529@gmail.com>
2021-12-16 22:44:24 +0600

    Added create-item page

--- pages/index.js
+++ pages/index.js
@@ -82,7 +82,7 @@ export default function Home() {
                 </div>
                 <div className="p-4 bg-black">
                   <p className="text-2xl mb-4 font-bold text-white">{nft.price} MATIC</p>
-                  <button className="w-full bg-pink-500 text-white font-bold py-2 px-12 rounded" onClick={() => buyNft(nft)}>Buy</button>
+                  <button className="w-full bg-blue-500 text-white font-bold py-2 px-12 rounded" onClick={() => buyNft(nft)}>Buy</button>
                 </div>
               </div>
             ))
